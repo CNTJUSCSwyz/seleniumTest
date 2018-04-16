33cd6074531f8f9befc9314ad1d3a1b8b2719edd
wyz <wyz_tjuscs@126.com>
2018-04-17 00:52:30 +0800

    增加了测试比率的计算

--- solve.py
+++ solve.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 from selenium import webdriver
-import time
 import requests
 import os
 import sys
@@ -8,6 +7,15 @@ import re
 import argparse
 from openpyxl import load_workbook
 users=[]
+global url ,flagtrue,flagfalse,flagerror
+flagerror=0
+flagfalse=0
+flagtrue=0
+options=webdriver.ChromeOptions()
+options.set_headless()
+# options.add_argument('--headless')
+options.add_argument('--disable-gpu')
+browser = webdriver.Chrome(options=options)
 def getUser(path):
     wb=load_workbook(path)
     sheet = wb.worksheets[0]
@@ -18,36 +26,37 @@ def getUser(path):
     return users
 def check(user):
     if user[0]!=None and user[1]!=None:
-        global url 
-        options=webdriver.ChromeOptions()
-        options.set_headless()
-        # options.add_argument('--headless')
-        options.add_argument('--disable-gpu')
-        browser = webdriver.Chrome(options=options)
+        global flagerror,flagfalse,flagtrue
         browser.get(url)
-        browser.refresh()
         username = browser.find_element_by_id("username")
         password = browser.find_element_by_id('password')
         username.send_keys(str(user[0]))
         password.send_keys(str(user[0])[-6:])
-        sys.stdout.flush()
         browser.find_element_by_id("submitButton").click()
         div = browser.find_element_by_tag_name("div")
         x = div.find_element_by_class_name("login-box-body")
         a = x.find_element_by_tag_name("a")
         if user[1] == a.get_attribute('href'):
             print "%s is OK" %(user[0])
+            flagtrue=flagtrue+1
         elif regux(user[1]) == regux(a.get_attribute('href')):
             print "%s is OK ,expected: %s ,but: %s" %(user[0],user[1],a.get_attribute('href'))
+            flagerror=flagerror+1
         else:
             print "%s is not OK !!!!! %s is not equal to %s" %(user[0],user[1],a.get_attribute('href'))
+            flagfalse=flagfalse+1
         sys.stdout.flush()
-        browser.close()
     else:
         pass
 def regux(filed): 
     ans = re.findall(".*github.com/([^ /])",filed)
     return ans
+def out(flagerror,flagfalse,flagtrue):
+    print ('----------------------------------------------')
+    print "Truth Percent: %.2f%s" %(flagtrue*100.0/(flagerror+flagfalse+flagtrue),'%')
+    print "Error Percent: %.2f%s" %(flagerror*100.0/(flagerror+flagfalse+flagtrue),'%')
+    print "False Percent: %.2f%s" %(flagfalse*100.0/(flagerror+flagfalse+flagtrue),'%')
+    print ('----------------------------------------------')
 def main(argv):
     parser = argparse.ArgumentParser(description='Test github Id')
     
@@ -76,18 +85,23 @@ def main(argv):
     if len(sys.argv) < 2 or args.f==None:
 		parser.print_help()
     else:
+        global flagtrue,flagfalse,flagerror
         path = args.f
         url = args.u
         payload = getUser(path)
         try:
             if args.i:
                 check(payload[args.i-1]) 
+                out(flagerror,flagfalse,flagtrue)
             elif args.w:
                 for user in payload[args.w-1:]:
                     check(user)
+                out(flagerror,flagfalse,flagtrue)
             else :
                 for user in payload:
                     check(user)
+                out(flagerror,flagfalse,flagtrue)
+            browser.close()
         except:
 			pass
             
